bb72fc87b3a1a723fe1d120889bcd00bcd74373b
Igbalabanov <101612094+Chs000-00@users.noreply.github.com>
2024-10-30 13:43:12 -0700

    More Bindings

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,25 @@
 namespace py = pybind11;
 using namespace anl;
 
-
+// Would autogenerating these instead be better?
 PYBIND11_MODULE(pyanlib, m) {
+    // util.h Functions
+    // Might not need to add these
     m.def("hermite_blend", &hermite_blend);
     m.def("quintic_blend", &quintic_blend);
+    m.def("fast_floor", &fast_floor);
+    m.def("array_dot", &array_dot);
+    m.def("array_dot3", &array_dot3);
+    m.def("bias", &bias);
+    m.def("gain", &gain);
+
+    // imaging.h
+    m.def("map2D", &map2D);
+    m.def("map2DNoZ", &map2DNoZ);
+    m.def("map3D", &map3D);
+
+
+
 
 
 #ifdef VERSION_INFO
